dc83342091e43d6a4ca1e3f2d701246b82e8bfe9
darkosel3 <darkosel7@gmail.com>
2024-12-02 19:18:00 +0100

    database connection

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ const Note = require('./models/note');
 
 app.use(express.json())
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
 
   const PORT = process.env.PORT || 3001
   app.listen(PORT, ()=>{
@@ -32,8 +15,8 @@ let notes = [
   })
 
   app.get('/api/notes', (request, response) => {
-     Note.find({}).then(notes =>{
-      response.json(notes)
+     Note.find({}).then(notee =>{
+      response.json(notee)
     })
   })
 
